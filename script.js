3afe5ffe41b6a0d19805d9a6af1d11016f734a60
Rohan Bhatia <rohanb459@gmail.com>
2022-05-21 16:27:08 +0530

    basic structuring done

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 let addBtn = document.querySelector(".add-btn");
+let modalCont = document.querySelector(".modal-container");
+let addFlag = false;
+addBtn.addEventListener("click", (e)=>{
+    // generate modal
+    
+    // if addflag = true -> display modal
+    // addFlag = false -> modal none
+    addFlag = !addFlag;
+    if(addFlag){
+        modalCont.style.display = "flex";
+    }
+    else{
+        modalCont.style.display = "none";
+    }
+})
