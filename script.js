667183e1ab391bde32cd0370f1a0f2b75348d029
Rohan Bhatia <rohanb459@gmail.com>
2022-05-21 18:07:48 +0530

    tickets generated randomly

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let addBtn = document.querySelector(".add-btn");
 let modalCont = document.querySelector(".modal-container");
+let mainCont = document.querySelector(".main-container")
+let textareaCont = document.querySelector(".textarea-cont")
 let addFlag = false;
 addBtn.addEventListener("click", (e)=>{
     // generate modal
@@ -14,3 +16,27 @@ addBtn.addEventListener("click", (e)=>{
         modalCont.style.display = "none";
     }
 })
+
+modalCont.addEventListener("keydown", (e)=>
+{
+    let key = e.key;
+    if(key=="Shift")
+    {
+        createTicket();
+        modalCont.style.display = "none";
+        addFlag = false;
+        textareaCont.value = "";
+    }
+})
+
+function createTicket(){
+    let ticketCont = document.createElement("div");
+    ticketCont.setAttribute("class", "ticket-cont");
+    ticketCont.innerHTML = `
+    <div class="ticket-color"></div>
+            <div class="ticket-id">#sample_id</div>
+            <div class="task-area">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis temporibus nisi culpa quidem id doloremque distinctio sequi. Amet nisi consequatur earum facere, a nostrum explicabo. Nesciunt odit omnis aut maxime.</div>
+            `;
+
+    mainCont.appendChild(ticketCont);
+}
