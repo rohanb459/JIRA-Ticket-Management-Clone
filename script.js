9378dd54bd03caa52638e99d7208eaf25e2180fb
Rohan Bhatia <rohanb459@gmail.com>
2022-05-22 23:30:51 +0530

    added script functionalities of lock and unlock

--- script.js
+++ script.js
@@ -2,7 +2,19 @@ let addBtn = document.querySelector(".add-btn");
 let modalCont = document.querySelector(".modal-container");
 let mainCont = document.querySelector(".main-container")
 let textareaCont = document.querySelector(".textarea-cont")
+let allPriorityColors = document.querySelectorAll(".priority-color")
+let removeBtn = document.querySelector(".remove-btn");
+let lockElem = document.querySelector(".ticket-lock");
+
+
+let colors = ["pr-1", "pr-2", "pr-3", "pr-4"]
+let modalPriorityColor = "pr-4";
 let addFlag = false;
+let removeFlag = false;
+
+let lockClass = "fa-lock";
+let unlockClass = "fa-lock-open";
+
 addBtn.addEventListener("click", (e)=>{
     // generate modal
     
@@ -17,26 +29,76 @@ addBtn.addEventListener("click", (e)=>{
     }
 })
 
+
+removeBtn.addEventListener("click", (e)=>{
+    removeFlag = !removeFlag;
+})
+
+
+// Listener for modal priority coloring
+allPriorityColors.forEach((colorElem, idx)=>{
+    colorElem.addEventListener("click", (e)=>{
+         allPriorityColors.forEach((priorityColorElem, idx)=>{
+             priorityColorElem.classList.remove("border");
+         })
+         colorElem.classList.add("border");
+         modalPriorityColor = colorElem.classList[0];
+    })
+})
+
 modalCont.addEventListener("keydown", (e)=>
 {
     let key = e.key;
     if(key=="Shift")
     {
-        createTicket();
+        createTicket(modalPriorityColor, textareaCont.value, shortid());
         modalCont.style.display = "none";
         addFlag = false;
         textareaCont.value = "";
     }
 })
 
-function createTicket(){
+function createTicket(ticketColor, ticketTask, ticketID){
     let ticketCont = document.createElement("div");
     ticketCont.setAttribute("class", "ticket-cont");
     ticketCont.innerHTML = `
-    <div class="ticket-color"></div>
-            <div class="ticket-id">#sample_id</div>
-            <div class="task-area">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis temporibus nisi culpa quidem id doloremque distinctio sequi. Amet nisi consequatur earum facere, a nostrum explicabo. Nesciunt odit omnis aut maxime.</div>
-            `;
-
+    <div class="ticket-color ${ticketColor}"></div>
+    <div class="ticket-id">#${ticketID}</div>
+    <div class="task-area">${ticketTask}</div>
+    <div class="ticket-lock">
+        <i class="fa-solid fa-lock"></i>
+    </div>
+    `;
     mainCont.appendChild(ticketCont);
+
+    handleLock(ticketCont);
+    handleRemoval(ticketCont);
+}
+
+
+
+function removeTicket(ticket)
+{
+    // removes ticket
+    // removeFlag is true then removes ticket 
+    if(removeFlag)
+    ticket.remove();
+}
+
+function handleLock(ticket)
+{
+    let ticketLockElem = ticket.querySelector(".ticket-lock");
+    let ticketLock = ticketLockElem.children[0];
+    ticketLock.addEventListener("click", (e)=>{
+        if(ticketLock.classList.contains(lockClass))
+        {
+            ticketLock.classList.remove(lockClass);
+            ticketLock.classList.add(unlockClass);
+        }
+        else
+        {
+            ticketLock.classList.remove(unlockClass);
+            ticketLock.classList.add(lockClass);
+        }
+    })
 }
